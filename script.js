567f2c9d193b38fdc933dfb7b74496ac0b571751
TajRask <roqayah1994@gmail.com>
2022-12-27 22:49:15 +0300

    gitUser

--- script.js
+++ script.js
@@ -1,16 +1,49 @@
+const APIURL = 'https://api.github.com/users/'
 const gitName= document.getElementById('name');
 const gitPic=document.getElementById('pic');
 const gitBio=document.getElementById('bio');
 const gitBtnSearch=document.getElementById('search');
 const gitSearchTerm=document.getElementById('search-term')
+const gitForm=document.getElementById("user-form")
 
 
-async function fetchMealById(gitSearchTerm) {
-    const idResp = await fetch("https://www.themealdb.com/api/json/v1/1/lookup.php?i=" + gitSearchTerm);
-    const idname = await idResp.json();
-    const name = idname.meals[0]
-    return name
 
-    alert(name)
+gitUser();
+gitInfo();
+async function gitUser(name) {
 
+    try{
+        const data = await fetch("https://www.themealdb.com/api/json/v1/1/lookup.php?i=" + name)
+
+        gitInfo(data)
+        
+    }catch{
+        if(err.response.status == 404) {
+            createErrorCard('No profile with this username')
+        }
+    }
+
+
+}
+
+function gitInfo(data){
+    const user =document.createElement('div')
+    user.classList.add('meal')
+    user.innerHTML=`
+    <div>
+
+            <div class="picture">
+                <img src="${data.avatar_url}">
+
+                <h1 ${data.login}</h1>
+            </div>
+
+
+            <div class="bio" id="bio">
+                <p>${data.bio}.</p>
+            
+
+        </div>
+    `
+console.log(data)
 }
