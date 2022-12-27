8e8b4079f36bde7208586fa09795adb218e0aaef
TajRask <roqayah1994@gmail.com>
2022-12-27 19:23:17 +0300

    fetch

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 const gitName= document.getElementById('name');
 const gitPic=document.getElementById('pic');
 const gitBio=document.getElementById('bio');
+const gitBtnSearch=document.getElementById('search');
+const gitSearchTerm=document.getElementById('search-term')
+const gitContainer=document.getElementById('main-container')
 
 
+gitContainer.addEventListener('submit',function(e)  {
+    e.preventDefult()
+    gitBtnSearch.value
+    console(gitBtnSearch)
 
-async function fetchUserName(id){
-    const idResp=await fetch("https://api.github.com/users/%7Buser%7D" +id)
-    const idName= await idResp.json();
-    const name=idName.id;
-    console.log(name)
-}
+})
