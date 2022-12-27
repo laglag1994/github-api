2391991d5c0cd41618ab630badfaa9a0c2af6d6d
TajRask <roqayah1994@gmail.com>
2022-12-27 19:09:24 +0300

    sctipt

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-const gitName= document.getElementById('name')
+const gitName= document.getElementById('name');
+const gitPic=document.getElementById('pic');
+const gitBio=document.getElementById('bio');
+
+
+
+async function fetchUserName(id){
+    const idResp=await fetch("https://api.github.com/users/%7Buser%7D" +id)
+    const idName= await idResp.json();
+    const name=idName.id;
+    console.log(name)
+}
