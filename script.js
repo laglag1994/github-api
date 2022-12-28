99520675baf8d3ac3cf6d63420af8291f08cf4e1
TajRask <roqayah1994@gmail.com>
2022-12-28 19:06:09 +0300

    yas

--- script.js
+++ script.js
@@ -1,54 +1,48 @@
-const gitForm = document.getElementById('form')
-const gitSearch = document.getElementById('search')
-const gitCard=document.getElementById('card')
+const gitSearchBtn = document.getElementById('search')
+const gitSerachTerm=document.getElementById('search-term')
+const gitMain=document.getElementById('main')
 
 
-
-gitUser();
-gitInfo();
 
 
 
 async function gitUser(name) {
-        const data = await fetch("https://api.github.com/users/" + name)
-        
-        gitInfo(await data.json())
+        const data = await (await fetch("https://api.github.com/users/" + name)).json()
+        console.log(data)
+        gitInfo(data)
+
 
 }
 
 
+gitSearchBtn.addEventListener('click', (e) => {
 
-function gitInfo(user) {
-    const cardHTML = `
-        <div class="card">
-            <div>
-                <img class="picture" src="${user.avatar_url}" alt="${user.name}" />
-            </div>
-            <div class="user-info">
-                <h2>${user.name}</h2>
-                <p>${user.bio}</p>
-                
-            </div>
-        </div>
-    `;
+    const username = gitSerachTerm.value
 
-    gitCard.innerHTML = cardHTML;
-}
+console.log(username)
+
+
+        gitUser(username)
+
+        gitSerachTerm.value = ''
+    
+})
+
 
 
 function gitInfo(data) {
+    gitMain.innerHTML=''
+    const user=document.createElement('div')
+    user.classList.add('user')
     
-    
-    const user = document.createElement('div')
-    user.classList.add('card')
-    gitCard=user.innerHTML = `
-
+    user.innerHTML= `
+        
             <div class="picture">
                 <img
                  src="${data.avatar_url}"
                  alt="${data.login}"
                  />
-                <h1 ${data.login}</h1>
+                <h1> ${data.login}</h1>
             </div>
 
 
@@ -57,29 +51,8 @@ function gitInfo(data) {
              </div>
 
     `
-    console.log(gitCard)
+
+    gitMain.append(user)
 }
 
 
-
-// function gitErr(errMsg){
-//     errMsg.innerHTML=`<h2>${errMsg}</h3>`
-// }
-
-
-
-
-
-
-
-form.addEventListener('submit', (e) => {
-    e.preventDefault()
-
-    const user = gitSearch.value
-
-    if (user) {
-        gitUser(user)
-
-        gitSearch.value = ''
-    }
-})
